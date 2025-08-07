49b1ad5b6eddffa6a0ca77a6fea34aef3797e2ac
Boran Uzun <boran.u3@gmail.com>
2025-08-07 17:38:21 +0200

    updated bootloader configuration

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -7,8 +7,10 @@
   ];
 
   # Boot configuration
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+  boot.loader.grub.device = "/dev/sda";
+  boot.loader.efi.canTouchEfiVariables = false;
 
   # Network configuration
   networking.hostName = "nixos";
