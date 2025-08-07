4c82bcfd9e2f2137c70587a7fac98e4da2f4dc2f
Boran Uzun <boran.u3@gmail.com>
2025-08-07 18:09:03 +0200

    removed disko and boot loader grub version (no effect anymore)

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -8,7 +8,6 @@
 
   # Boot configuration
   boot.loader.grub.enable = true;
-  boot.loader.grub.version = 2;
   boot.loader.grub.device = "/dev/sda";
   boot.loader.efi.canTouchEfiVariables = false;
 
